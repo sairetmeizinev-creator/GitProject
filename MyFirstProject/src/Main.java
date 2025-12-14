ec7c32e7fca9f5465fe8c0f84e37d4fc39f6bd05
sairetmeizinev-creator <sairet.meizi@gmail.com>
2025-12-14 03:00:11 +0200

    Updated 2 Main.java

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello svqt na java!");
+        System.out.println("Hello svqt na java i ne samo!");
     }
 }
